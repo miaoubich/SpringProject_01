8657e7d01a2078b12fea38d779fe37458248c6f1
ALI <alibouzar@foi.hr>
2020-01-06 14:32:10 +0100

    Project setup

---
+++ mvcproject01backend/mvcproject01backend/src/test/java/net/darinlina/mvcproject01backend/AppTest.java
@@ -0,0 +1,20 @@
+package net.darinlina.mvcproject01backend;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
